2b4ae507eb38cacefca962662de2e64673b7d3bc
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-04-27 17:41:37 +0900

    重複している商品名を解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
-before_action :search_product, only: [:index, :search]
+  before_action :search_product, only: [:index, :search]
 
   def index
     @products = Product.all
+    set_product_column
   end
 
   def search
@@ -12,5 +13,9 @@ before_action :search_product, only: [:index, :search]
   private
   def search_product
     @p = Product.ransack(params[:q])
+  end
+
+  def set_product_column
+    @product_name = Product.select("name").distinct
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/search'
   root 'products#index'
   get 'products/search'
 end
